6e814c59ac46e2720da17c7e3a7f7fc3a845f8fc
JairBarco <jair_barco@outlook.com>
2021-09-02 00:11:53 -0500

    Parte 5

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -1,16 +1,39 @@
 package Conexion;
 
-import com.sun.jdi.connect.spi.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class Conexion{
+public class Conexion {
+
     private String db = "punto_de_ventas";
     private String user = "root";
     private String password = "";
     private String urlMysql = "jdbc:mysql://localhost/" + db + "?SslMode=none";
     private String urlSql = "jdbc:sqlserver://localhost:1433;databaseName=" + db + ";integratedSecurity=true;";
+
     private Connection conn = null;
-    
-    public Conexion(){
-        
+
+    public Conexion() {
+        try {
+            //Obtener driver MySQL
+            //Class.forName("com.mysql.jdbc.Driver");
+            //conn = DriverManager.getConnection(this.urlMysql, this.user, this.password);
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(urlSql);
+
+            if (conn != null) {
+                System.out.println("Conexion a la base de datos " + this.db + "... Listo");
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Error: " + ex);
+        }
+
     }
+
+    public Connection getConn() {
+        return conn;
+    }
+
 }
